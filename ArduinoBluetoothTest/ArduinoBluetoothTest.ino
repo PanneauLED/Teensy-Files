7c649286b9a70ebeb2d5f211e57a534ff2a9b36f
Nick Anthony <nicholas_anthony@brown.edu>
2016-05-24 17:06:20 +0200

    Updated Bluetooth Test

--- ArduinoBluetoothTest/ArduinoBluetoothTest.ino
+++ ArduinoBluetoothTest/ArduinoBluetoothTest.ino
@@ -1,25 +1,25 @@
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
   Serial1.begin(115200);
 
 }
 
+// put your main code here, to run repeatedly: 
 void loop() {
   String t;
   while(Serial1.available()){
-    t += (char) Serial1.read();
+    t += (char) Serial.read();
   }
-  
   if(t.length()){
+    Serial.print("Received");
     if (t == "hello"){
+      
       Serial1.print("Hello from the bluetooth module");
     }
     else{
       Serial1.print("Sorry, I don't know that");
     }
-    
   }
   delay(20);
-  // put your main code here, to run repeatedly: 
-  
 }
