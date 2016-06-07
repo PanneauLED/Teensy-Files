70615dcf4381fcdceba5513de7070c1471da2cf6
Nick Anthony <nicholas_anthony@brown.edu>
2016-06-07 22:39:44 +0200

    Website Updates

--- Programme_pour_bluetooth/LEDLetter.cpp
+++ Programme_pour_bluetooth/LEDLetter.cpp
@@ -50,6 +50,34 @@ void LEDLetter::place(CRGB ledPanel[6][256], Panel connection){
   FastLED.show();
 }
 
+void LEDLetter::make1(){
+  _shapeArray[0][0] = 0;
+  _shapeArray[0][1] = 2;
+  _shapeArray[1][0] = 0;
+  _shapeArray[1][1] = 3;
+  _shapeArray[2][0] = 0;
+  _shapeArray[2][1] = 4;
+  _shapeArray[3][0] = 1;
+  _shapeArray[3][1] = 1;
+  _shapeArray[4][0] = 1;
+  _shapeArray[4][1] = 3;
+  _shapeArray[5][0] = 2;
+  _shapeArray[5][1] = 0;
+  _shapeArray[6][0] = 2;
+  _shapeArray[6][1] = 3;
+  _shapeArray[7][0] = 3;
+  _shapeArray[7][1] = 1;
+  _shapeArray[8][0] = 3;
+  _shapeArray[8][1] = 3;
+  _shapeArray[9][0] = 4;
+  _shapeArray[9][1] = 2;
+  _shapeArray[10][0] = 4;
+  _shapeArray[10][1] = 3;
+  _shapeArray[11][0] = 4;
+  _shapeArray[11][1] = 4;
+  
+}
+
 void LEDLetter::makeA(){
   _shapeArray[0][0] = 0;
   _shapeArray[0][1] = 2;
